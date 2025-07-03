2d030842f58982c94cd0f2913741cefd581bb743
mygameworld9 <3043527855@qq.com>
2025-07-03 11:25:42 +0800

    Roll with invulnerability.

--- Script/player.gd
+++ Script/player.gd
@@ -1,100 +1,150 @@
 extends CharacterBody2D
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -150.0   # 初始起跳速度，稍微调低点
-const MAX_JUMP_HOLD_TIME = 0.2 # 最大跳跃维持时间（单位：秒）
-const EXTRA_JUMP_FORCE = -1000.0 # 持续按住跳跃键时的额外向上力
+const JUMP_VELOCITY = -150.0
+const MAX_JUMP_HOLD_TIME = 0.2
+const EXTRA_JUMP_FORCE = -1000.0
 const MAX_JUMP = 2
 
+@export var ROLL_SPEED_MULTIPLIER = 2.0
+@export var ROLL_DURATION = 0.8
+@export var ROLL_COLLISION_HEIGHT_MULTIPLIER = 0.5
+
+@export var poppable_tilemap_layer_index: int = 0
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var roll_timer: Timer = $Timer
 @onready var head_hitbox: Area2D = $HeadHitbox
 @onready var head_ray: RayCast2D = $HeadRay
 @onready var game: Node2D = $".."
 @onready var mainground: TileMapLayer = $"../MainGround"
-@onready var animated_sprite = $AnimatedSprite2D
 @onready var gamemanage: CanvasLayer = $"../GameManage"
-@export var poppable_tilemap_layer_index: int = 0 
 
-var coin_spawned_tiles = {}
-var is_jumping = false          # 是否正在跳跃
-var jump_time = 0.0             # 持续按住跳跃键的时间
+var is_rolling = false
+var is_invulnerable = false
+var is_jumping = false
+var jump_time = 0.0
 var jumps = MAX_JUMP
+var coin_spawned_tiles = {}
+
+var original_collision_shape_extents: Vector2
+var original_collision_shape_position: Vector2
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	roll_timer.timeout.connect(_on_roll_timer_timeout)
+	animated_sprite.animation_finished.connect(_on_animation_finished)
+
+	if collision_shape and collision_shape.shape is RectangleShape2D:
+		original_collision_shape_extents = collision_shape.shape.extents
+		original_collision_shape_position = collision_shape.position
+	else:
+		push_warning("Main collision_shape is not RectangleShape2D or missing!")
+
 func _physics_process(delta):
-	# 如果在地面，重置跳跃状态
+	if is_rolling:
+		var target_speed = (-1 if animated_sprite.flip_h else 1) * SPEED * ROLL_SPEED_MULTIPLIER
+		velocity.x = move_toward(velocity.x, target_speed,delta)  # 改这里，越小加速越慢		if not is_on_floor():
+		velocity.y += gravity * delta
+		move_and_slide()
+		return
+	
 	if is_on_floor():
 		is_jumping = false
-		jumps = 2
+		jumps = MAX_JUMP
 		jump_time = 0.0
+
 	gamemanage.point()
-	# 添加重力
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# 按下跳跃键（刚开始跳）
+	if Input.is_action_just_pressed("roll") and is_on_floor() and not is_rolling:
+		print("Roll triggered")
+		start_roll()
+
 	if Input.is_action_just_pressed("jump") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
 		jumps -= 1
 		is_jumping = true
 		jump_time = 0.0
 
-	# 如果正在跳跃，并且继续按住跳跃键，并且没超过允许跳跃时间
 	if is_jumping and Input.is_action_pressed("jump") and jump_time < MAX_JUMP_HOLD_TIME:
 		velocity.y += EXTRA_JUMP_FORCE * delta
 		jump_time += delta
 	else:
 		is_jumping = false
 
-	# 获取左右移动方向（-1/0/1）
 	var direction = Input.get_axis("left", "right")
 
-	# 翻转精灵朝向
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-
-	# 播放动画
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle") 
+	if not is_rolling:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-	
-	# 设置水平速度
+			animated_sprite.play("jump")
+	print("播放动画：", animated_sprite.animation)
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	#if head_ray.is_colliding and head_ray.get_collider() is TileMapLayer:
-		#spawn_coin()
-	move_and_slide()
-#func spawn_coin():
-	#var tilemap = head_ray.get_collider()
-	##print(2333)
-	#var hit_pos = head_ray.get_collision_point()
-	##print(hit_pos)
-	#var cell = tilemap.local_to_map(hit_pos)
-	#print(cell)
-	#var tile_data = tilemap.get_cell_tile_data(cell)
-	#var tile_center_local_pos = tilemap.map_to_local(cell) + tilemap.tile_set.tile_size / 2.0
-	#var coin = load("res://background/Coin.tscn")
-	#var coin_instance = coin.instantiate() 
-	#
-	#game.add_child(coin_instance) 
-	#coin_instance.global_position = tile_center_local_pos + Vector2(-8,-33)
-	##coin_instance.position = tilemap.to_local(tile_center_local_pos)
-	## 触发金币的弹出动画/行为 (假设 Coin.gdd  脚本中有 pop_up() 方法)
-	#if coin_spawned_tiles.has(cell):
-		#if coin_instance.has_method("pop_up"):
-			#coin_instance.pop_up()
-	#tilemap.set_cell(3, cell, 2)
 
-	#var mouse_position = get_global_mouse_position()
-	#var	 tile_coordinate = mainground.local_to_map(mouse_position)
-	#var clicked_cell = mainground.local_to_map(mainground.get_local_mouse_position())
-	#var data = mainground.get_cell_tile_data(clicked_cell)
-	#if data:
-		 #if data.get_custom_data("coin"):	# 应用移动
+	move_and_slide()
+
+# --- ROLLING ---
+func start_roll():
+	print("Start roll triggered")
+	#print("start_roll() called!")
+	is_rolling = true
+	is_invulnerable = true
+	animated_sprite.play("roll")
+
+	var current_horizontal_input = Input.get_axis("left", "right")
+	if current_horizontal_input != 0:
+		animated_sprite.flip_h = (current_horizontal_input < 0)
+
+	roll_timer.start(ROLL_DURATION)
+
+	if collision_shape and collision_shape.shape is RectangleShape2D:
+		var rect_shape = collision_shape.shape
+		rect_shape.extents = Vector2(original_collision_shape_extents.x, original_collision_shape_extents.y * ROLL_COLLISION_HEIGHT_MULTIPLIER)
+		collision_shape.position = Vector2(original_collision_shape_position.x, original_collision_shape_position.y + original_collision_shape_extents.y * (1.0 - ROLL_COLLISION_HEIGHT_MULTIPLIER))
+func wudi():
+	return is_invulnerable
+func end_roll():
+	is_rolling = false
+	is_invulnerable = false
+
+	if collision_shape and collision_shape.shape is RectangleShape2D:
+		var rect_shape = collision_shape.shape
+		rect_shape.extents = original_collision_shape_extents
+		collision_shape.position = original_collision_shape_position
+
+	velocity.x = 0
+
+	var current_direction = Input.get_axis("left", "right")
+	if is_on_floor():
+		if current_direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+
+func _on_roll_timer_timeout():
+	if is_rolling:
+		end_roll()
+
+func _on_animation_finished(anim_name: String):
+	if anim_name == "roll":
+		if is_rolling and roll_timer.is_stopped():
+			end_roll()
---
+++ killzone_2.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+#@onready var game_manage: CanvasLayer = %GameManage
+
+@onready var death_wait_time = 0.25 # 等待时间
+@onready var gamemanage: CanvasLayer = get_node("/root/Game/GameManage")
+func _ready():
+	timer.wait_time = death_wait_time # 在 _ready() 中设置 Timer 的等待时间
+	timer.one_shot = true # 确保只触发一次
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and not body.wudi():
+		# 禁用玩家的物理处理、输入处理和视觉
+		body.set_physics_process(false)
+		body.set_process(false)
+		body.set_process_input(false)
+		body.visible = false
+		# 2改变玩家的碰撞层和掩码
+		# 假设你的玩家的碰撞层通常在第1层，敌人检测第1层。
+		body.set_collision_mask(0)  # 让玩家不检测任何层
+		# 死亡处理
+		gamemanage.player_die()
+
+		# 减慢时间
+		Engine.time_scale = 0.2
+		# 重新加载场景
+		timer.start()
+
+
+func _on_timer_timeout():
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
